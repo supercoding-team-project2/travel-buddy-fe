59cc5e1e43dd2489452e243e6558ff19546b810a
JeongminJamie <sksrhcalsu@gmail.com>
2024-07-12 00:19:44 +0900

    created dropbox

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,33 +1,6 @@
-import classNames from "classnames/bind";
-import styles from "./Header.module.css";
-
-import Image from "next/image";
-import account from "../../assets/account.png";
-import chat from "../../assets/chat.png";
-
-//home&explore Link로 바꾸기 및 useRouter 사용해 현재 페이지의 nav 디자인 바꾸기
-// import Link from "next/link";
-
-const cx = classNames.bind(styles);
+"use client";
+import { HeaderClient } from "./HeaderClient";
 
 export function Header() {
-  return (
-    <div className={cx("Header")}>
-      <div className={cx("company-container")}>
-        <div className={cx("company-logo")}>Travel Buddy</div>
-      </div>
-      <div className={cx("navigatation-user-container")}>
-        <div className={cx("header-nav")}>Home</div>
-        <div className={cx("header-nav")}>Explore</div>
-        <button className={cx("login-button")}>Log In</button>
-        <button className={cx("signup-button")}>Sign Up</button>
-        <Image
-          className={cx("account-icon")}
-          src={account}
-          alt="account-icon"
-        />
-        <Image className={cx("chat-icon")} src={chat} alt="chat-icon" />
-      </div>
-    </div>
-  );
+  return <HeaderClient />;
 }
