4f1f8ac1f70c9713dffa3e223562a1b9ea3170d7
JeongminJamie <sksrhcalsu@gmail.com>
2024-07-11 14:19:48 +0900

    testy

--- app/(Pages)/SignIn/page.tsx
+++ app/(Pages)/SignIn/page.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames/bind';
 import styles from './SignIn.module.css';
-
+console.log("테스ㅔ트다")
 const cx = classNames.bind(styles);
 
 export default function SignIn(){
