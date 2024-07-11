09142b4ed8640f0e4568800409b01e9736b4dde0
gongjuyeonhee <yyeonhee912@naver.com>
2024-07-12 00:37:52 +0900

    edit footer css

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import classNames from "classnames/bind";
 import styles from "./Footer.module.css";
 import Link from "next/link";
+import Image from "next/image";
 
 const cx = classNames.bind(styles);
 
@@ -8,19 +9,34 @@ export function Footer() {
   return (
     <>
       <div className={cx("Footer")}>
-        <div className={cx("Footer-name")}>travelBuddy</div>
+        <div className={cx("Footer-name-container")}>
+          <div className={cx("Footer-name")}>travelBuddy</div>
+          <div className={cx("Footer-github")}>
+            <a
+              href="https://github.com/supercoding-team-project2/travel-buddy-fe"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={cx("Footer-github")}
+            >
+              <Image
+                src="/github.svg"
+                alt="GitHub Logo"
+                width={30}
+                height={30}
+              />
+            </a>
+          </div>
+        </div>
+
         <div className={cx("Footer-link-container")}>
           <Link href="/" className={cx("link-home")}>
             Home
           </Link>
           <Link href="/" className={cx("link-guide")}>
-            가이드
-          </Link>
-          <Link href="/" className={cx("link-together")}>
-            동행
+            Explore
           </Link>
           <div className={cx("copyright")}>
-            © 2024 travelBuddy. All Rights Reserved.
+            © Copyright 2024. All Rights Reserved.
           </div>
         </div>
       </div>
