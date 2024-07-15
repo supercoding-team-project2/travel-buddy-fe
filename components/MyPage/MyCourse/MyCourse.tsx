3268040994369d2669e0f519d78097cc7dcaaa75
JeongminJamie <sksrhcalsu@gmail.com>
2024-07-15 16:03:45 +0900

    added empty course

--- components/MyPage/MyCourse/MyCourse.tsx
+++ components/MyPage/MyCourse/MyCourse.tsx
@@ -7,13 +7,14 @@ import Image from "next/image";
 import upArrow from "../../../assets/up-arrow.png";
 
 import EachCourse from "./EachCourse";
+import EmptyMyCourse from "./EmptyMyCourse";
 
 import { useState } from "react";
 const cx = classNames.bind(styles);
 
 const MyCourse = () => {
   //테스트 데이터 배열
-  const testData = [0, 1, 2];
+  const testData: any = [];
   const [openIndex, setOpenIndex] = useState<number | null>(null);
 
   //하나의 여행 경로 컴포넌트가 클릭 되었을 때, 나머지는 다 닫아놓기
@@ -22,29 +23,39 @@ const MyCourse = () => {
   };
 
   return (
-    <main className={cx("my-course-container")}>
-      <div
-        className={cx("upArrow-container")}
-        onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-      >
-        <div className={cx("upArrow-word")}>Top</div>
-        <Image src={upArrow} alt="up-arrow" className={cx("upArrow-icon")} />
-      </div>
-      <div className={cx("course-button-container")}>
-        <button className={cx("course-button")}>내 경로 생성하기</button>
-      </div>
-      <div className={cx("courses-container")}>
-        {testData.map((element, index) => {
-          return (
-            <EachCourse
-              key={index}
-              isCourseOpen={index === openIndex}
-              clickEachHandler={() => clickEachCourseHandler(index)}
+    <>
+      {testData.length === 0 ? (
+        <EmptyMyCourse />
+      ) : (
+        <main className={cx("my-course-container")}>
+          <div
+            className={cx("upArrow-container")}
+            onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+          >
+            <div className={cx("upArrow-word")}>Top</div>
+            <Image
+              src={upArrow}
+              alt="up-arrow"
+              className={cx("upArrow-icon")}
             />
-          );
-        })}
-      </div>
-    </main>
+          </div>
+          <div className={cx("course-button-container")}>
+            <button className={cx("course-button")}>내 경로 생성하기</button>
+          </div>
+          <div className={cx("courses-container")}>
+            {testData.map((element: any, index: number) => {
+              return (
+                <EachCourse
+                  key={index}
+                  isCourseOpen={index === openIndex}
+                  clickEachHandler={() => clickEachCourseHandler(index)}
+                />
+              );
+            })}
+          </div>
+        </main>
+      )}
+    </>
   );
 };
 
