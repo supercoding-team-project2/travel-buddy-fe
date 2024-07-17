3e34bf3c2547ea1f2c8bcd08c30d9a7ad8f4a573
JeongminJamie <sksrhcalsu@gmail.com>
2024-07-17 13:42:07 +0900

    added input on search

--- components/Course/Map/Places.tsx
+++ components/Course/Map/Places.tsx
@@ -3,10 +3,18 @@ import React from "react";
 import classnames from "classnames/bind";
 import styles from "./Places.module.css";
 
+import Image from "next/image"
+import search from "@/assets/search.png"
+
 const cx = classnames.bind(styles);
 
 const Places = () => {
-  return <div className={cx("places-container")}>places를 두는 공간</div>;
+  return <div className={cx("places-container")}>
+    <div className={cx("search-container")}>
+    <input className={cx("search-input")} placeholder="여행 장소 검색하기"/>
+    <Image src={search} alt="search" className={cx("search-icon")}/>
+    </div>
+    <div>places를 두는 공간</div></div>;
 };
 
 export default Places;
