0a2e552b9aed9aef526972d0602a8c4e28ea8264
gongjuyeonhee <yyeonhee912@naver.com>
2024-07-18 19:11:58 +0900

    Data Binding & Data Mapping-DetailsTable

--- app/(Pages)/PostDetail/clientComponent.tsx
+++ app/(Pages)/PostDetail/clientComponent.tsx
@@ -7,7 +7,78 @@ import TravelBar from "@/components/PostDetail/TravelBar";
 import ProfilePost from "@/components/PostDetail/profilePost";
 import { IconButton } from "@/components/PostDetail/iconButton";
 
-const InfoTable = () => {
+/*경로바-TravelBar */
+const locations = [
+  {
+    date: "2024년 7월 18일",
+    location: [
+      {
+        name: "용용선생",
+        description: "restaurant",
+      },
+      {
+        name: "스타벅스",
+        description: "cafe",
+      },
+      { name: "대박장소", description: "locality" },
+      {
+        name: "레전드맛집",
+        description: "restaurant",
+      },
+      { name: "신라호텔", description: "hotel" },
+    ],
+  },
+  {
+    date: "2024년 7월 18일",
+    location: [
+      {
+        name: "용용선생",
+        description: "restaurant",
+      },
+      {
+        name: "스타벅스",
+        description: "cafe",
+      },
+      { name: "대박장소", description: "locality" },
+      {
+        name: "레전드맛집",
+        description: "restaurant",
+      },
+      {
+        name: "레전드맛집",
+        description: "restaurant",
+      },
+      {
+        name: "레전드맛집",
+        description: "restaurant",
+      },
+      {
+        name: "레전드맛집",
+        description: "restaurant",
+      },
+      {
+        name: "레전드맛집",
+        description: "restaurant",
+      },
+      {
+        name: "레전드맛집",
+        description: "restaurant",
+      },
+      { name: "신라호텔", description: "hotel" },
+    ],
+  },
+];
+
+/*참가인원-DetailsTable */
+const participations = [
+  {
+    age: [{ min: 20, max: 30 }],
+    number: 20,
+    gender: "무관",
+  },
+];
+
+const InfoTable = ({}) => {
   return (
     <section className="text-gray-600 body-font">
       <div className="container mx-auto flex px-5 pt-24 pb-10 md:flex-row flex-col items-center">
@@ -26,25 +97,32 @@ const InfoTable = () => {
   );
 };
 
-const DetailsTable = () => {
+const DetailsTable = ({ participations }: any) => {
   return (
     <div>
-      <table className="min-w-80 bg-white border border-gray-200">
-        <tbody>
-          <tr>
-            <td className="py-2 px-4 border-b">연령</td>
-            <td className="py-2 px-4 border-b">20~30</td>
-          </tr>
-          <tr>
-            <td className="py-2 px-4 border-b">참여인원</td>
-            <td className="py-2 px-4 border-b">4명</td>
-          </tr>
-          <tr>
-            <td className="py-2 px-4 border-b">성별</td>
-            <td className="py-2 px-4 border-b">무관</td>
-          </tr>
-        </tbody>
-      </table>
+      {participations.map((participation: any, index: number) => (
+        <table key={index} className="min-w-80 bg-white border border-gray-200">
+          <tbody>
+            <tr>
+              <td className="py-2 px-4 border-b">연령</td>
+
+              {participation.age.map((age: any, index: number) => (
+                <td key={index} className="py-2 px-4 border-b">
+                  {age.min}~{age.max} 대
+                </td>
+              ))}
+            </tr>
+            <tr>
+              <td className="py-2 px-4 border-b">참여인원</td>
+              <td className="py-2 px-4 border-b">{participation.number}명</td>
+            </tr>
+            <tr>
+              <td className="py-2 px-4 border-b">성별</td>
+              <td className="py-2 px-4 border-b">{participation.gender}</td>
+            </tr>
+          </tbody>
+        </table>
+      ))}
     </div>
   );
 };
@@ -65,68 +143,6 @@ const TogetherBtn = ({ onClick }: any) => {
 const ClientComponent = () => {
   const router = useRouter();
   const { openModal, ModalWrapper } = useModal();
-
-  const locations = [
-    {
-      date: "2024년 7월 18일",
-      location: [
-        {
-          name: "용용선생",
-          description: "restaurant",
-        },
-        {
-          name: "스타벅스",
-          description: "cafe",
-        },
-        { name: "대박장소", description: "locality" },
-        {
-          name: "레전드맛집",
-          description: "restaurant",
-        },
-        { name: "신라호텔", description: "hotel" },
-      ],
-    },
-    {
-      date: "2024년 7월 18일",
-      location: [
-        {
-          name: "용용선생",
-          description: "restaurant",
-        },
-        {
-          name: "스타벅스",
-          description: "cafe",
-        },
-        { name: "대박장소", description: "locality" },
-        {
-          name: "레전드맛집",
-          description: "restaurant",
-        },
-        {
-          name: "레전드맛집",
-          description: "restaurant",
-        },
-        {
-          name: "레전드맛집",
-          description: "restaurant",
-        },
-        {
-          name: "레전드맛집",
-          description: "restaurant",
-        },
-        {
-          name: "레전드맛집",
-          description: "restaurant",
-        },
-        {
-          name: "레전드맛집",
-          description: "restaurant",
-        },
-        { name: "신라호텔", description: "hotel" },
-      ],
-    },
-  ];
-
   return (
     <>
       <div className="flex flex-col space-y-4 m-6">
@@ -154,7 +170,7 @@ const ClientComponent = () => {
         <div className="flex justify-around">
           <InfoTable />
           <div className="flex flex-col pt-24  ">
-            <DetailsTable />
+            <DetailsTable participations={participations} />
             <div className="flex items-center justify-center mt-4">
               {/* 현재 참여인원받아서 뿌려주는걸로 */}
               <TogetherBtn onClick={openModal} />
