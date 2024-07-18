c01673b150f53b58055e20e0d0e5ee4a3a43b84a
gongjuyeonhee <yyeonhee912@naver.com>
2024-07-18 15:46:37 +0900

    make into components-postView-cilentComponent

--- app/(Pages)/PostView/clientComponent.tsx
+++ app/(Pages)/PostView/clientComponent.tsx
@@ -59,24 +59,37 @@ export const SelectPost = () => {
 
 export const ClientComponent = () => {
   return (
-    <div>
-      <DatePickerWithRange />
-    </div>
-  );
-};
+    <div className={cx("post-container")}>
+      <div className={cx("post-top-container")}>
+        <div className={cx("button-container")}>
+          <div className={cx("category-button-group")}>
+            <ButtonOutline text="전체" />
+            <ButtonOutline text="후기" />
+            <ButtonOutline text="동행" />
+            <ButtonOutline text="가이드" />
+          </div>
+          <div className={cx("view-button-group")}>
+            <ButtonOutline text="추천한 게시물" />
+            <ButtonOutline text="참여한 여행" />
+          </div>
+        </div>
 
-export const PostComponent = () => {
-  return (
-    <div>
+        <div className={cx("select-write-group")}>
+          <div className={cx("select-container")}>
+            <SelectPost />
+            <DatePickerWithRange />
+          </div>
+
+          <div className={cx("write-icon-container")}>
+            <WriteButton />
+          </div>
+        </div>
+      </div>
       <PostCard />
     </div>
   );
 };
 
 export default {
-  SelectPost,
-  ButtonOutline,
-  WriteButton,
   ClientComponent,
-  PostComponent,
 };
--- app/(Pages)/PostView/page.tsx
+++ app/(Pages)/PostView/page.tsx
@@ -1,45 +1,7 @@
-import {
-  SelectPost,
-  ButtonOutline,
-  WriteButton,
-  ClientComponent,
-  PostComponent,
-} from "./clientComponent";
-import styles from "./PostView.module.css";
-import classNames from "classnames/bind";
-const cx = classNames.bind(styles);
+import { ClientComponent } from "./clientComponent";
 
 const page = () => {
-  return (
-    <div className={cx("post-container")}>
-      <div className={cx("post-top-container")}>
-        <div className={cx("button-container")}>
-          <div className={cx("category-button-group")}>
-            <ButtonOutline text="전체" />
-            <ButtonOutline text="후기" />
-            <ButtonOutline text="동행" />
-            <ButtonOutline text="가이드" />
-          </div>
-          <div className={cx("view-button-group")}>
-            <ButtonOutline text="추천한 게시물" />
-            <ButtonOutline text="참여한 여행" />
-          </div>
-        </div>
-
-        <div className={cx("select-write-group")}>
-          <div className={cx("select-container")}>
-            <SelectPost />
-            <ClientComponent />
-          </div>
-
-          <div className={cx("write-icon-container")}>
-            <WriteButton />
-          </div>
-        </div>
-      </div>
-      <PostComponent />
-    </div>
-  );
+  return <ClientComponent />;
 };
 
 export default page;
