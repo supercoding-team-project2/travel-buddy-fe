6d40842c667859079166e0d5e7ab57eeb9165793
JeongminJamie <sksrhcalsu@gmail.com>
2024-07-22 17:54:45 +0900

    on eachCourse axios

--- components/MyPage/MyCourse/EachCourse.tsx
+++ components/MyPage/MyCourse/EachCourse.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 import classNames from "classnames/bind";
 import styles from "./EachCourse.module.css";
@@ -7,36 +8,87 @@ import Image from "next/image";
 import edit from "../../../assets/edit.png";
 import bin from "../../../assets/bin.png";
 
-import { useState } from "react";
 import CourseDeleteModal from "./CourseDeleteModal";
 
 const cx = classNames.bind(styles);
 
 interface Props {
+  id: number;
+  name: string;
+  startDate: string;
+  endDate: string;
+  createdAt: string;
   isCourseOpen: boolean;
   clickEachHandler: () => void;
 }
 
-const EachCourse: React.FC<Props> = ({ isCourseOpen, clickEachHandler }) => {
+const EachCourse: React.FC<Props> = ({
+  id,
+  name,
+  startDate,
+  endDate,
+  createdAt,
+  isCourseOpen,
+  clickEachHandler,
+}) => {
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
   const [isHovered, setIsHovered] = useState(false);
+  const [detailData, setDetailData] = useState();
+
+  //현재 id가 열려있을 때, axios get 요청
+  useEffect(() => {
+    if (isCourseOpen) {
+      const accessToken = localStorage.getItem("accessToken");
+
+      if (accessToken) {
+        axios
+          .get("url", { headers: { Authorization: `Bearer ${accessToken}` } })
+          .then((response) => {
+            console.log("여행 경로 디테일 조회 데이터", response.data);
+            setDetailData(response.data);
+          })
+          .catch((error) => {
+            console.error("여행 경로 디테일 조회 요청 실패", error);
+          });
+      }
+    }
+  }, [isCourseOpen]);
 
   return (
     <>
-      <CourseDeleteModal isDeleteOpen={isDeleteModalOpen} setIsDeleteOpen={setIsDeleteModalOpen}/>
-      <div className={cx("each-course-container", {"each-course-container-active": isHovered})} onClick={clickEachHandler}>
+      <CourseDeleteModal
+        isDeleteOpen={isDeleteModalOpen}
+        setIsDeleteOpen={setIsDeleteModalOpen}
+      />
+      <div
+        className={cx("each-course-container", {
+          "each-course-container-active": isHovered,
+        })}
+        onClick={clickEachHandler}
+      >
         <div className={cx("show-course")}>
-          <div className={cx("course-name")} onMouseEnter={()=>setIsHovered(true)} onMouseLeave={()=>setIsHovered(false)}>여행 이름</div>
-          <div className={cx("course-period")}>
-            2024년 7월 10일 ~ 2024년 7월 15일
+          <div
+            className={cx("course-name")}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          >
+            {name}
           </div>
-          <div className={cx("course-posted-date")}>2024/07/02</div>
+          <div className={cx("course-period")}>
+            {startDate} ~ {endDate}
+          </div>
+          <div className={cx("course-posted-date")}>{createdAt}</div>
           <div className={cx("course-icons-container")}>
             <div className={cx("image-conatiner")}>
               <Image src={edit} alt="edit" className={cx("edit-icon")} />
             </div>
             <div className={cx("image-conatiner")}>
-              <Image src={bin} alt="bin" className={cx("bin-icon")} onClick={() => setIsDeleteModalOpen(true)}/>
+              <Image
+                src={bin}
+                alt="bin"
+                className={cx("bin-icon")}
+                onClick={() => setIsDeleteModalOpen(true)}
+              />
             </div>
           </div>
         </div>
--- components/MyPage/MyCourse/index.tsx
+++ components/MyPage/MyCourse/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 import classNames from "classnames/bind";
 import styles from "./MyCourse.module.css";
@@ -9,25 +10,63 @@ import upArrow from "../../../assets/up-arrow.png";
 import EachCourse from "./EachCourse";
 import EmptyMyCourse from "./EmptyMyCourse";
 
-import { useState } from "react";
 import { useRouter } from "next/navigation";
 const cx = classNames.bind(styles);
 
 const MyCourse = () => {
   const router = useRouter();
+  const [openId, setOpenId] = useState<number | null>(null);
 
   //테스트 데이터 배열
-  const testData: any = [];
-  const [openIndex, setOpenIndex] = useState<number | null>(null);
+  const [postData, setPostData] = useState([
+    {
+      id: 1,
+      name: "여행 이름1",
+      startDate: "2024년 7월 10일",
+      endDate: "2024년 7월 11일",
+      createdAt: "2024/07/02",
+    },
+    {
+      id: 2,
+      name: "여행 이름2",
+      startDate: "2024년 7월 12일",
+      endDate: "2024년 7월 13일",
+      createdAt: "2024/07/03",
+    },
+    {
+      id: 3,
+      name: "여행 이름3",
+      startDate: "2024년 7월 14일",
+      endDate: "2024년 7월 15일",
+      createdAt: "2024/07/04",
+    },
+  ]);
 
   //하나의 여행 경로 컴포넌트가 클릭 되었을 때, 나머지는 다 닫아놓기
-  const clickEachCourseHandler = (index: number) => {
-    setOpenIndex(index === openIndex ? null : index);
+  const clickEachCourseHandler = (id: number) => {
+    setOpenId(id === openId ? null : id);
   };
+
+  // 경로 조회 axios get 요청
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+
+    if (accessToken) {
+      axios
+        .get("url", { headers: { Authorization: `Bearer ${accessToken}` } })
+        .then((response) => {
+          console.log("경로 조회 데이터", response.data);
+          setPostData(response.data);
+        })
+        .catch((error) => {
+          console.error("경로 조회 요청 실패", error);
+        });
+    }
+  }, []);
 
   return (
     <>
-      {testData.length === 0 ? (
+      {postData.length === 0 ? (
         <EmptyMyCourse />
       ) : (
         <main className={cx("my-course-container")}>
@@ -51,12 +90,17 @@ const MyCourse = () => {
             </button>
           </div>
           <div className={cx("courses-container")}>
-            {testData.map((element: any, index: number) => {
+            {postData.map((element: any, index: number) => {
               return (
                 <EachCourse
-                  key={index}
-                  isCourseOpen={index === openIndex}
-                  clickEachHandler={() => clickEachCourseHandler(index)}
+                  key={element.id}
+                  id={element.id}
+                  name={element.name}
+                  startDate={element.startDate}
+                  endDate={element.endDate}
+                  createdAt={element.createdAt}
+                  isCourseOpen={element.id === openId}
+                  clickEachHandler={() => clickEachCourseHandler(element.id)}
                 />
               );
             })}
