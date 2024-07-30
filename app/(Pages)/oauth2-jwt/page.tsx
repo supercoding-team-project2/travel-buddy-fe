c839033ceb122bbf9d36a046c7af13d1f63e52e0
hyung9jun0204@gmail.com <gimdawon@gimdawon-ui-MacBookPro.local>
2024-07-31 03:50:02 +0900

    qwerqwer

--- app/(Pages)/oauth2-jwt/page.tsx
+++ app/(Pages)/oauth2-jwt/page.tsx
@@ -72,7 +72,7 @@
 
 'use client';
 
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 export default function OAuth2RedirectHandler() {
