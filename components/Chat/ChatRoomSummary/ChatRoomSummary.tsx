a8628d6b4fb3a83cce0601a48cd58263c9be712e
hyung9jun0204@gmail.com <gimdawon@gimdawon-ui-MacBookPro.local>
2024-08-02 00:47:32 +0900

    fix/online-logic

--- components/Chat/ChatRoomSummary/ChatRoomSummary.tsx
+++ components/Chat/ChatRoomSummary/ChatRoomSummary.tsx
@@ -7,7 +7,7 @@ export function ChatRoomSummary({ roomId, opponentName, status, setChatRoomId }:
   return (
     <div className={cx('ChatRoomSummary')} onClick={() => setChatRoomId(roomId)}>
       <div className={cx('opponentName')}>{opponentName}</div>
-      <div className={cx('online')}>현재 활동 중</div>
+      {status === 'ONLINE' ? <div className={cx('online')}>현재 활동 중</div> : <></>}
     </div>
   );
 }
